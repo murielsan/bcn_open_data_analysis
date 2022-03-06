6b85c853dab2f5cb72b83f99da20834d9f718e29
murielsan <murielsan@gmail.com>
2022-03-06 16:20:57 +0100

    Finished new model import function

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from routers import air_quality_data
-import uvicorn
 
 app = FastAPI()
 
@@ -11,7 +10,3 @@ app.include_router(air_quality_data.router)
 @app.get("/")
 async def root():
     return {"message":"BCN open data analysis API. Endpoints info: https://github.com/murielsan/bcn_open_data_analysis/blob/main/README.md"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
