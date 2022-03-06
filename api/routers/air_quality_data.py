a48d93972cfca6671f8d3e8eb98e568e5bfc67f0
murielsan <murielsan@gmail.com>
2022-03-06 19:12:58 +0100

    Added insert new measure (POST method)

--- api/routers/air_quality_data.py
+++ api/routers/air_quality_data.py
@@ -1,8 +1,9 @@
 from tokenize import String
 from fastapi import APIRouter
-from database.mongo import get_data, insert_one_data, distinct
 from bson import json_util
 from json import loads
+from database.mongo import get_data, insert_one_data, distinct
+from models.Measure import Measure
 
 # Population endpoint
 router = APIRouter()
@@ -11,3 +12,17 @@ router = APIRouter()
 def get_station_list():
    res = distinct('pollution','Station')
    return loads(json_util.dumps(res))
+
+@router.get("/stations/{name}")
+def get_station_list(name):
+   res = get_data('pollution',filter={'Station':name},project={'Station':1,'Location':1,'District Name':1,'Neighborhood name':1,'_id':0},limit=1)
+   return loads(json_util.dumps(res[0]))
+
+# Insert new measure, according to Measure class
+@router.post("/insert/measure")
+async def insert_measure(measure:Measure):
+   # Check if already inserted by station and hour of the day
+   if len(get_data("pollution", filter={'Station':measure.station, 'Hour':measure.hour, 'Year':measure.year, 'Month':measure.mont, 'Day':measure.day})) == 0:
+      inserted = insert_one_data(measure)
+      return {"message":"Measure inserted correctly", "id":str(inserted.inserted_id)}
+   return {"message":"Data already in the database"}
