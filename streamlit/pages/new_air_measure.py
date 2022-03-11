9b67b88205d182b751185f7d2709bfffcde999a3
murielsan <murielsan@gmail.com>
2022-03-11 19:15:02 +0100

    Changed form

--- streamlit/pages/new_air_measure.py
+++ streamlit/pages/new_air_measure.py
@@ -7,24 +7,23 @@ def new_air_measure():
     st.title("Upload new data")
     stations = get_stations_list()
 
-    with st.form(key='new_air_measure_form'):
-        st.write("Please, fill the form:")        
-        col1, col2, col3 = st.columns([2,2,3])
-        with col1:        
-            dt = st.date_input("Date of the measure", max_value=date.today())
-        with col2:
-            hour = st.number_input("Hour", min_value=0, max_value=23)
-        with col3:
-            station = st.selectbox("Measure station",stations)
+    st.write("Please, fill the form:")        
+    col1, col2, col3 = st.columns([2,2,3])
+    with col1:        
+        dt = st.date_input("Date of the measure", max_value=date.today())
+    with col2:
+        hour = st.number_input("Hour", min_value=0, max_value=23)
+    with col3:
+        station = st.selectbox("Measure station",stations)
 
-        col4, col5, col6 = st.columns(3)
-        with col4:
-            o3 = st.number_input("O3", value=0.0)
-        with col5:
-            no2 = st.number_input("NO2", value=0.0)
-        with col6:
-            pm10 = st.number_input("PM10", value=0.0)
-        
-        # Every form must have a submit button.
-        if st.form_submit_button("Submit"):
-            set_new_measure(station, hour, dt.year, dt.month, dt.day, o3, no2, pm10)
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        o3 = st.number_input("O3", value=0.0)
+    with col5:
+        no2 = st.number_input("NO2", value=0.0)
+    with col6:
+        pm10 = st.number_input("PM10", value=0.0)
+    
+    # Every form must have a submit button.
+    if st.button("Submit"):
+        set_new_measure(station, hour, dt.year, dt.month, dt.day, o3, no2, pm10)
